b38094e362a3d9182e19e6b47a850fdfa61eb7a3
bulharualina <bulgarualina3@gmail.com>
2025-05-31 19:23:02 +0300

    Main Menu

---
+++ Game/Assets/Art/Character/Scripts/MenuManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance { get; set; }
+
+    public GameObject menuCanvas;
+    public GameObject uiCanvas;
+    public GameObject menu;
+    public GameObject saveMenu;
+    public GameObject settingsMenu;
+
+
+    public bool isOpen;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && !isOpen)
+        {
+            uiCanvas.SetActive(false);
+            menuCanvas.SetActive(true);
+            isOpen = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.M) && isOpen)
+        {
+            saveMenu.SetActive(false);
+            settingsMenu.SetActive(false);
+            menu.SetActive(true);
+
+            uiCanvas.SetActive(true);
+            menuCanvas.SetActive(false);
+            isOpen = false;
+        }
+    }
+}
